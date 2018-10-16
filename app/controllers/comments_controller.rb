d3cd451be991c2eed1b6344f2090a113eb1be00d
Brad Dong <braddong@hotmail.com>
2018-10-17 10:30:40 +1100

    cannot have blank comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,20 +7,27 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @current_comment = Comment.create comment:params[:comment]
-    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    puts @current_comment.comment
-    @current_comment.user_id = @current_user.id
-    @current_comment.save
+    if params[:comment].present?
+      @current_comment = Comment.create comment: params[:comment]
+      @current_comment.user_id = @current_user.id  #assign the id from @current_user to new var
+      @current_comment.save
 
-    if @current_comment.persisted?
-      conflict = Conflict.find params[:conflict_id].to_i
-      @current_user.comments << @current_comment
-      conflict.comments << @current_comment
-      conflict.save
-      @current_user.save
+      if @current_comment.persisted?
+        @conflict = Conflict.find params[:conflict_id].to_i
+        @current_user.comments << @current_comment    #user to comment association
+        @conflict.comments << @current_comment #conflict to comment association
+        @conflict.save
+        @current_user.save
 
-      redirect_to conflict_path(conflict.id)
+        redirect_to conflict_path(@conflict.id)
+      end
+    else
+      puts "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      puts 'comment field empty'
+      @conflict = Conflict.find params[:conflict_id].to_i
+      flash[:error] = 'Comments field cannot be empty.'
+      puts flash[:error]
+      redirect_to conflict_path(@conflict.id)
     end
   end
 
