6f3a020244bfd4be4c263e2319f3681c5d90bc71
Brad Dong <braddong@hotmail.com>
2018-10-19 10:04:06 +1100

    showing landing page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to=>"static_pages#home"
+  root :to =>"static_pages#home"
 
 
   resources :users
